1569245c112b7444e1500310e3ac46a9108e678d
Dom-Nega <domnega92@gmail.com>
2024-12-02 20:39:31 -0800

    updating files for asteroids project: Circleshape, constants, main and player

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ import pygame
 # import everything from a module
 # into the current file
 from constants import *
-
+from player import *
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    color = (255,255,255)
+    
     print("Starting asteroids!")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    color = (0,0,0)
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -19,8 +23,11 @@ def main():
                 return
         screen.fill(color)
         pygame.display.flip()
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+        Player.draw(screen)
+
+        # limit the FPS to 60
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
